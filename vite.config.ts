ada2884dccbd660b38e70fa22c1a4b00cf631936
Christoph Gockel <git@christophgockel.de>
2026-07-30 19:00:04 +0100

    Adds and configures dependencies

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig(({ command }) => ({
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      formats: ["es"],
+      fileName: "index",
+    },
+    rolldownOptions: {
+      external: ["fflate", "saxes", /^node:/],
+    },
+  },
+  // No need to use this plugin when running tests
+  plugins: command === "build" ? [dts({ include: ["src"] })] : [],
+  test: {
+    include: ["test/**/*.test.ts"],
+    watch: false,
+    environment: "node",
+  },
+}));
